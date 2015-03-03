1faccc880d547c2d2e5bc20dcb81c3d03b2166c0
Rafael <rafayotuel@gmail.com>
2015-03-02 20:42:12 -0600

    Social auth login

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '801249783243420',
+		'clientSecret' 	: 'fe70dc2b4ea92d5e6059387048904f47',
+		'callbackURL' 	: 'http://rafa.com:9000/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'GTMPYt0fNYBSiKTb4yVK2OjPE',
+		'consumerSecret' 	: 'rWIoVi0twDbQWuD9e6XWxATmXOn2q8PnLypqUJsucBfj1V0fuf',
+		'callbackURL' 		: 'http://rafa.com:9000/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '334565532238-pi78tt7dk2tig0jk9a1k282k8ofp00fj.apps.googleusercontent.com',
+		'clientSecret' 	: 'CVC486TvikljseyqWQmCJoAG',
+		'callbackURL' 	: 'http://rafa.com:9000/auth/google/callback'
+	}
+
+};
